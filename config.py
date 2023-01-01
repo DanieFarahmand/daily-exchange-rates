f81f4b4c48abc37fc8a6d04e13b1c23041478356
danie <danie.0098.sh@gmail.com>
2023-01-01 21:31:32 +0330

    sms notification added to project

--- config.py
+++ config.py
@@ -6,7 +6,14 @@ API_KEY = API_KEY
 rules = {
     "archive": True,
     "email": {
-        "enable": True,
+        "enable": False,
         "preferred": ["USD", "IRR", "EUR", "BTC", "EUR"]
+    },
+    "notification": {
+        "enable": True,
+        "preferred": {
+            "USD": {"min": 1.03, "max": 1.08},
+            "IRR": {"min": 44800, "max": 44900},
+            "BTC": {"min": 6.46, "max": 6.48}}
     }
 }
--- main.py
+++ main.py
@@ -18,14 +18,27 @@ def archive(timestamp, rates):
         f.write(json.dumps(rates))
 
 
-def send_mail(subject, rates):
+def send_mail(timestamp, rates):
     exchanges = dict()
     if rules["email"]["preferred"] is not None:
         for exc in rules["email"]["preferred"]:
             exchanges[exc] = rates[exc]
 
     text = json.dumps(exchanges)
-    send_smtp_email(text, subject)
+    send_smtp_email(text, timestamp)
+
+
+def send_notification(rates):
+    preferred = rules["notification"]["preferred"]
+    msg = ""
+    for exc in preferred.keys():
+
+        if rates[exc] >= preferred[exc]["max"]:
+            msg += f"{exc} reached max: {rates[exc]} \n"
+
+        if rates[exc] <= preferred[exc]["min"]:
+            msg += f"{exc} reached min: {rates[exc]} \n"
+    print(msg)
 
 
 if __name__ == "__main__":
@@ -36,3 +49,6 @@ if __name__ == "__main__":
 
     if rules["email"]["enable"]:
         send_mail(res["timestamp"], res["rates"])
+
+    if rules["notification"]["enable"]:
+        send_notification(res["rates"])
